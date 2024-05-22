6e6d42f7b847f5a5b36f2d9dff3311817df8f367
smanaman <sanvatsman@gmail.com>
2024-05-22 05:16:22 -0700

    codec

--- q1.c
+++ q1.c
@@ -1,7 +1,25 @@
+// Q.1 Write a Program to find the reverse of a 1D array using a Chain of Pointers.
+// For example,
+// Input:
+// Enter the array's size: 5
+
+
+// Enter array elements:
+// a[0] = 5
+// a[1] = 9  
+// a[2] = 4
+// a[3] = 7
+// a[4] = 3
+
+// Output:
+// Reversed array elements:
+// 3, 7, 4, 9, 5
 #include<stdio.h>
-void fudu(int *arr,int *size){
-  for(int i = *size-1;i>=0;i--){
-printf("%d\n",*arr + i);
+
+void fudu(int *arr,int size){
+    printf("Your revers string is\n");
+  for(int i = size-1;i>=0;i--){
+printf("%d\n",*(arr + i));
   }
 }
 int main(){
@@ -15,6 +33,6 @@ scanf("%d",&size);
         scanf("%d",&arr[i]);
     }   
     
-    fudu(arr,&size);
+    fudu(arr,size);
 
 }
